c2947ec1769ade9d80ca450053049118475a6832
Ulvis <nutzys33@gmail.com>
2023-10-27 12:15:52 +0300

    gray color added

--- util/colors.js
+++ util/colors.js
@@ -1,7 +1,8 @@
 export const COLORS = {
-    primary: "#884A39",
-    secondary: "#C38154",
-    accent: "#FFC26F",
-    back: "#F9F6EE",
-    front: "#FAF9F6",
-}
+  primary: "#884A39",
+  secondary: "#C38154",
+  accent: "#FFC26F",
+  back: "#F9F6EE",
+  front: "#FAF9F6",
+  gray: "#808080",
+};
