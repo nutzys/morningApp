76ff0371038dfbf76d36b59c86cdd05b1d507ceb
Ulvis <nutzys33@gmail.com>
2023-10-27 11:31:41 +0300

    post context added

--- store/post-context.js
+++ store/post-context.js
@@ -3,11 +3,15 @@ import { createContext, useState } from "react";
 export const PostContext = createContext({
   addTitle: () => {},
   addContent: () => {},
+  setUid: () => {},
+  setImgUrl: () => {},
 });
 
 const PostContextProvider = ({ children }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [userId, setUserId] = useState("");
 
   const addTitle = (input) => {
     setTitle(input);
@@ -17,19 +21,31 @@ const PostContextProvider = ({ children }) => {
     setContent(input);
   };
 
+  const setUid = (input) => {
+    setUserId(input);
+  };
+
+  const setImgUrl = (input) => {
+    setImageUrl(input);
+  };
+
   const createPost = () => {
     const post = {
+      userId: userId,
       title: title,
       content: content,
       createdAt: new Date(Date.now()),
-      imageUrl: "",
-      likeCount: "0",
+      imageUrl: imageUrl,
+      likeCount: 0,
     };
+    createPost(post);
   };
 
   const values = {
     addTitle: addTitle,
     addContent: addContent,
+    setUid: setUid,
+    setImgUrl: setImgUrl,
   };
 
   return <PostContext.Provider value={values}>{children}</PostContext.Provider>;
