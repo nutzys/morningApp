c81ea623d77a691ac90f738ae44de84df81ff1f7
Ulvis <nutzys33@gmail.com>
2023-10-28 12:47:00 +0300

    post context cleanup

--- store/post-context.js
+++ store/post-context.js
@@ -4,39 +4,24 @@ import { createImage } from "../api/createImage";
 import { UserContext } from "./user-context";
 
 export const PostContext = createContext({
-  addTitle: () => {},
-  addContent: () => {},
   create: () => {},
 });
 
 const PostContextProvider = ({ children }) => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
   const userCtx = useContext(UserContext);
 
-  const addTitle = (input) => {
-    setTitle(input);
-  };
-
-  const addContent = (input) => {
-    setContent(input);
-  };
-
-  const create = async (images) => {
+  const create = async (title, content, images) => {
     const result = await createPost(
-      userCtx.token.user.id,
+      userCtx.user.id,
       title,
       content,
       new Date()
     );
-    let post = result.data.posts;
-    console.log(post);
-    createImage(post.id, images);
+    console.log(result.data);
+    //createImage(result.id, images);
   };
 
   const values = {
-    addTitle: addTitle,
-    addContent: addContent,
     create: create,
   };
 
